6091f645c07039a1124f9229df943e3d7596d00a
zhouyuexie <zhouyuexie@gmail.com>
2016-11-24 14:35:25 +0800

    已经成功,但是算法时间度比较大,无法通过测试,正在修改

--- leetcode/3LSWRC.js
+++ leetcode/3LSWRC.js
@@ -1,9 +1,9 @@
-var log = console.log;
+let log = console.log;
 
 // data is [],数组里的元素是否全部一样
 let isRepeat = (data)=>{
-	var datai = data[0];//记录第一个数据
-	for(var i=1;i<data.length;i++){
+	let datai = data[0];//记录第一个数据
+	for(let i=1;i<data.length;i++){
 		if(datai!==data[i]){
 			return false;//说明是这种"bbbbbba"
 		}
@@ -11,7 +11,7 @@ let isRepeat = (data)=>{
 	return true;//说明是这种"bbbbbbb"
 }
 
-// 删除重复的数据
+// 删除相连的重复的数据
 let deleteRepeatString = (data) =>{
 	let noRepeat = "";
 	for(let i=0;i<data.length;i++){
@@ -19,39 +19,104 @@ let deleteRepeatString = (data) =>{
 			noRepeat = data[i] + noRepeat;
 		}
 	}
-	log("deleteRepeatString:",noRepeat.split("").reverse().join(""))
 	return noRepeat.split("").reverse().join("");
+}
+
+//删除所有的重复数据
+let deleteAllRepeat = (data) =>{
+	let set = new Set();
+	let arrayData = data.split("");
+
+	arrayData.map((item)=>{
+		set.add(item);
+	});
+
+	let result = "";
+
+	set.forEach((item)=>{
+		result = result+item;
+	})
+	return result;
+}
+
+//一个都没有重复
+let isNoRepeat = (data) =>{
+	if(data!==deleteAllRepeat(data)){
+		//删除全部相同的字段,如果删除后和原来的相等,说明没有相同的字段
+		return false;
+	}
+	return true;
+}
+
+// 得到最大的数
+let getBig = (a,b) =>{
+	if(a>=b){
+		return a
+	}
+	else{
+		return b;
+	}
 }
 
 // 循环获取数组里所有不相等相连的字符串
 let getNoRepeatLength = (data) =>{
-	let arrayData = [];
+	let Big = 0;//最大的数
 	let length = data.length;
 	let head = 0;//从数组下标第一个开始比较
-	let diffLength = 2;//从长度为2开始比较,因为deleteRepeatString函数去掉了相邻相同的字符串了
+	let diffLength = 1;//从长度为2开始比较
 
-	//第一个开始,数组每个元素都要循环和它之后的元素就行对比
 	while(head!==length-1){
-		for(let i=head;i<length;i++){
-			//截取相同长度不同位置的字符串对比
-			//如果有相等的说明
-			if(data.substr(i,diffLength)===data.substr(i+1,diffLength)){
-				arrayData.push(diffLength);//找到目前最长的长度diffLength了
-				diffLength++;
-				head++;
-				break;
+		while(diffLength<=length-head){
+			let diffData = data.substr(head,diffLength);//获取所有的截取字段
+			if(diffData===deleteAllRepeat(diffData)){
+				//如果截取的字段和过滤过的字段相等
+				//说明里面没有相同的字段
+				Big = getBig(Big,diffData.length);//记录长度
 			}
 			else{
 				diffLength++;
-				head++;
+				continue;
 			}
+
+			if(isRepeat(diffData)){
+				//有相连相同的
+				//要在判断之前的字符串是否有和现在两个相同的字符串,比如这两个相同的字段是a,那么还要检测前面有没有这个a字段
+				let sampleData = data.substr(head,1);//相同的字段
+				let preData = data.substr(0,head);//之前的字符串
+
+				if(preData.indexOf(sampleData)===-1){
+					//之前的字符串没有这个相同的字段,也就是没有a
+					//那么需要处理
+					Big = getBig(Big,diffData.length+1);//记录长度
+				}
+				
+				// log(preData,sampleData);
+			}
+			else{
+				Big = getBig(Big,diffLength);//记录长度
+			}
+			// log(head,diffLength);
+			diffLength++;
 		}
+		diffLength = 1;//归位
+		head++;
 	}
-	return arrayData.sort()[arrayData.length-1];
+
+	//返回最大的	
+	return Big;
 }
 
 let lengthOfLongestSubstring = (string) => {
-	//首先验证是不是全部一样的字符
+	//首先验证空
+	if(string===""){
+		return 0;
+	}
+	//验证全部都不一样的字符串
+	if(isNoRepeat(string)){
+		//如果都不一样就直接返回
+		return string.length;
+	}
+	//验证是不是全部一样的字符
 	let arrayData = string.split("");
 	let deleteRepeat = deleteRepeatString(arrayData);
 	if(deleteRepeat.length===1){
@@ -63,4 +128,4 @@ let lengthOfLongestSubstring = (string) => {
 	}
 }
 
-log(lengthOfLongestSubstring("pwwkew"))
+log(lengthOfLongestSubstring("wjgmypyakesezwmgtyozouebthzsvuftgoiefg"))
