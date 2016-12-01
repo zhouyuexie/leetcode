8fdee18d366c66ef4e2108c3266f8b43948a0948
zhouyuexie <zhouyuexie@gmail.com>
2016-12-01 11:16:37 +0800

    增加注释

--- leetcode/4MOTSA.js
+++ leetcode/4MOTSA.js
@@ -1,15 +1,25 @@
+/**
+ * 入口
+ * @param  {[type]} nums1 [description]
+ * @param  {[type]} nums2 [description]
+ * @return {[type]}       [description]
+ */
 let findMedianSortedArrays = (nums1, nums2)=> {
-	let all = nums1.concat(nums2);
+	let all = nums1.concat(nums2);//连接两个数组
+	// 排序
 	let sortArray = all.sort((a,b)=>{
 		return a-b;
 	});
 
-	let len = sortArray.length;
+	let len = sortArray.length;//获取该数组长度
 	if(len%2===0){
-		// 偶数
+		// 数组为偶数
+		// 返回数组中间的两个数的平均数
 		return (sortArray[len/2-1]+sortArray[len/2])/2;
 	}
 	else{
+		// 数组为奇数
+		// 返回数组的中间值
 		return sortArray[parseInt(len/2)];
 	}
 };
