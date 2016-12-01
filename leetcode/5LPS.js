d948068b51247ea6310bac21c959fdd3a2c616ca
zhouyuexie <zhouyuexie@gmail.com>
2016-12-01 11:17:22 +0800

    运行到最后输入超长的字符串会超时,准备优化

--- leetcode/5LPS.js
+++ leetcode/5LPS.js
@@ -7,15 +7,19 @@ const isPalindromic = s => {
 	let len = s.length;
 	// 偶数
 	if(len%2===0){
-		let headValue = s[0];
-		for(let i=1;i<len-1;i++){
+		let head = 0;//第一个
+		let last = len-1;//最后一个
+
+		for(let i=0;i<len/2;i++){
 			// 判断是否相同
-			if(headValue!==s[i]){
-				//i之前的相同
-				return true;
+			if(s[head]!==s[last]){
+				//只要有一个不同就返回false
+				return false;
 			}
+			head++;
+			last--;
 		}
-		return false;
+		return true;
 	}
 	else{
 		// 奇数
@@ -35,29 +39,45 @@ const isPalindromic = s => {
 		return true;
 	}
 }
+
 /**
  * 入口
  * @param  {[type]} s [description]
  * @return {[type]}   [description]
  */
 const longestPalindrome = s => {
+	if(s.length===1)return s;//如果输入字符串长度为1就直接返回
+
 	let start = 0;//s开始地方
 	let head = 1;//指针
-	let len = s.length-1;
+	let len = s.length;
+	let result = [];//存储回文
 
-	while(start<=len){
-		let diff = len-start;
-		while(head<diff){
+	while(start<len){
+		let diff = len-start;//head可以移动的范围
+		while(head<=diff){
 			let substr = s.substr(start,head);
-			// if(isPalindromic(substr)){
-			// 	console.log(substr)
-			// }
-			console.log(substr)
+
+			if(isPalindromic(substr)){
+				result.push(substr);
+			}
+			// console.log(substr)
 			head++;
 		}
-		start++;
 		head=1;
+		start++;
+	}
+
+	// 如果一个都没有
+	if(result.length===0){
+		return s[0];//直接返回第一个
+	}
+	else{
+		// console.log(result)
+		return result.sort((a,b)=>{
+			return b.length-a.length;
+		})[0];//只返回第一个
 	}
 };
 
-longestPalindrome("cbbd")
+console.log(longestPalindrome("aaabaaaa"))
